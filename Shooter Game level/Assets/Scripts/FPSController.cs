08e041eb09aee0b2d3b5814412281b657b8f0d55
Muhammad Farhan <farhan.aqeel@hotmail.com>
2016-01-11 22:04:23 +0500

    Player leftRight movement

---
+++ Shooter Game level/Assets/Scripts/FPSController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSController : MonoBehaviour {
+
+    float forwardBackwardSpeed;
+    float leftRightSpeed;
+    CharacterController controller;
+    int movementSpeed = 5;
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	
+	void Update () {
+        forwardBackwardSpeed = Input.GetAxis("Vertical");
+        leftRightSpeed = Input.GetAxis("Horizontal");
+        Vector3 speed = new Vector3(leftRightSpeed, 0, forwardBackwardSpeed) * movementSpeed;
+
+        controller.SimpleMove(speed);
+	}
+}
