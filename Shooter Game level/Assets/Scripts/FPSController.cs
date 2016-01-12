b9295373f4ee7f10264175afa51c1803f82ff760
Muhammad Farhan <farhan.aqeel@hotmail.com>
2016-01-12 20:25:41 +0500

    head movement leftRight

--- Shooter Game level/Assets/Scripts/FPSController.cs
+++ Shooter Game level/Assets/Scripts/FPSController.cs
@@ -7,16 +7,20 @@ public class FPSController : MonoBehaviour {
     float leftRightSpeed;
     CharacterController controller;
     int movementSpeed = 5;
+    float leftRightRotation;
+
 	void Start () {
         controller = GetComponent<CharacterController>();
 	}
 	
 	
 	void Update () {
+        leftRightRotation = Input.GetAxis("Mouse X");
+        transform.Rotate(0,leftRightRotation,0);
         forwardBackwardSpeed = Input.GetAxis("Vertical");
         leftRightSpeed = Input.GetAxis("Horizontal");
         Vector3 speed = new Vector3(leftRightSpeed, 0, forwardBackwardSpeed) * movementSpeed;
-
+        speed = transform.rotation * speed;
         controller.SimpleMove(speed);
 	}
 }
