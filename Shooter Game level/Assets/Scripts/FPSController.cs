f66a3a380ba3a99f377f3bf75b562e1bd4309422
Muhammad Farhan <farhan.aqeel@hotmail.com>
2016-01-13 18:05:31 +0500

    Head movement done.

--- Shooter Game level/Assets/Scripts/FPSController.cs
+++ Shooter Game level/Assets/Scripts/FPSController.cs
@@ -7,16 +7,21 @@ public class FPSController : MonoBehaviour {
     float leftRightSpeed;
     CharacterController controller;
     int movementSpeed = 5;
-    float leftRightRotation;
-
-	void Start () {
+    float horizontalRotation;
+    float verticalRotation;
+    void Start () {
         controller = GetComponent<CharacterController>();
 	}
 	
 	
 	void Update () {
-        leftRightRotation = Input.GetAxis("Mouse X");
-        transform.Rotate(0,leftRightRotation,0);
+
+        verticalRotation -= Input.GetAxis("Mouse Y");
+        verticalRotation = Mathf.Clamp(verticalRotation, -50, 50);
+        this.GetComponentInChildren<Camera>().transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+        horizontalRotation = Input.GetAxis("Mouse X");
+        transform.Rotate(0,horizontalRotation,0);
         forwardBackwardSpeed = Input.GetAxis("Vertical");
         leftRightSpeed = Input.GetAxis("Horizontal");
         Vector3 speed = new Vector3(leftRightSpeed, 0, forwardBackwardSpeed) * movementSpeed;
