a23fd866916ae0ee561f0afcb58783ae0d32a0cb
Muhammad Farhan <farhan.aqeel@hotmail.com>
2016-01-29 22:26:59 +0500

    .

---
+++ Shooter Game level/Assets/Scripts/attackMissile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class attackMissile : MonoBehaviour {
+
+    public GameObject missile;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Instantiate(missile, GetComponentInChildren<Camera>().transform.position, GetComponentInChildren<Camera>().transform.rotation);
+        }
+
+	}
+}
---
+++ Shooter Game level/Assets/Scripts/missileSpeed.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class missileSpeed : MonoBehaviour {
+
+    float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(transform.forward * speed * Time.deltaTime, Space.World);
+	}
+}
